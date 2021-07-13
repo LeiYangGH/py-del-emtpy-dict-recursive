b5aca0d0474b31db9a3c0940e8efc90f72f52ab7
LeiYangGH <675686066@qq.com>
2021-07-13 20:55:52 +0800

    done

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import json
 from pprint import pprint
 from raw import data
 
-
 DELETE = 'D'
 
 
@@ -42,35 +41,33 @@ def visit_l(parent_d, l):
             deleted += mark_d_delete(d)
     return deleted
 
-def remove_d_if_marked(d):
-    pass
-    
-#
-# def delete_empty(parent_d, l):
-#     for d in l:
-#         if DELETE in d:
-#             l.remove(d)
-#
-#         sections = d['sections']
-#         if sections:
-#             if all(DELETE in se for se in sections):
-#                 deleted += mark_d_delete(d)
-#             for sec in sections:
-#                 print(sec['item'])
-#                 if DELETE in sec:
-#                     continue
-#                 if (not sec['sections']) and sec['value'] == 'None':
-#                     deleted += mark_d_delete(sec)
-#                 deleted += visit_l(sec, sec['sections'])
-#         elif d['value'] == 'None':
-#             deleted += mark_d_delete(d)
-#     return deleted
+
+def remove_d_if_marked(l):
+    deleted = False
+    index = len(l) - 1
+    while index >= 0:
+        if DELETE in l[index]:
+            del l[index]
+            deleted = True
+        index -= 1
+    return deleted
+
+
+def delete_empty(l):
+    remove_d_if_marked(l)
+    for d in l:
+        sections = d['sections']
+        if sections:
+            delete_empty(sections)
+
 
 d = 1
 while d > 0:
     print(d)
     d = visit_l(None, data)
-
+pprint(data)
+while delete_empty(data):
+    pass
 pprint(data)
 
 # print(json.dumps(data))
