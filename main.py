242f5b50eba68f340c42d12ffa0f157f3a49cef0
LeiYangGH <675686066@qq.com>
2021-07-13 17:59:59 +0800

    try del

--- main.py
+++ main.py
@@ -241,6 +241,28 @@ def visit_l(parent_d, l):
             deleted += mark_d_delete(d)
     return deleted
 
+def remove_d_if_marked(d):
+    
+
+def delete_empty(parent_d, l):
+    for d in l:
+        if DELETE in d:
+            l.remove(d)
+
+        sections = d['sections']
+        if sections:
+            if all(DELETE in se for se in sections):
+                deleted += mark_d_delete(d)
+            for sec in sections:
+                print(sec['item'])
+                if DELETE in sec:
+                    continue
+                if (not sec['sections']) and sec['value'] == 'None':
+                    deleted += mark_d_delete(sec)
+                deleted += visit_l(sec, sec['sections'])
+        elif d['value'] == 'None':
+            deleted += mark_d_delete(d)
+    return deleted
 
 d = 1
 while d > 0:
